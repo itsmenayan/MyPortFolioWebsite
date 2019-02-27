8769ae715a9d946a279e33a3b9d009d5f9385cac
Nayan <nayana@mindfiresolutions.com>
2019-02-27 15:32:42 +0530

    Fixing Hamburger Menu Animation for Mobile/Small Screens

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('toggleMenu', [
+      state('opened', style({
+        transform: 'translate3d(0, 0, 0)',
+        display:''
+        
+      })),
+      state('closed', style({
+        transform: 'translate3d(100%,0, 0)',
+        display:'none'
+       
+      })),
+      transition('opened => closed', animate('400ms ease-in-out')),
+      transition('closed => opened', animate('400ms ease-in-out'))
+    ]),
+  ],
 })
 export class NavbarComponent implements OnInit {
 
+ 
   constructor() { }
 
   ngOnInit() {
   }
 
+  menuState:string = 'closed';
+ 
+  toggleMenu() {
+    this.menuState = this.menuState === 'closed' ? 'opened' : 'closed';
+  }
+
+ 
+  
 }
+
